4e01d1c556887f30123b76a6db0758400044ccad
PMirdha <prateek1991mirdha@gmail.com>
2017-08-30 23:07:32 +0530

    Added Audio app to config file

--- vidSiteDjango/settings.py
+++ vidSiteDjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'audio.apps.AudiConfig',
     'video.apps.VideoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
