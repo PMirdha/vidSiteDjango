0e4c352abbd33c91f4566b2396e15c852891bb60
PMirdha <prateek1991mirdha@gmail.com>
2017-08-31 13:20:31 +0530

    Migrations of all table done for Audio App

--- audio/admin.py
+++ audio/admin.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import AudioTrackDetail,UserExtraDetail,OrderDetail
 
 # Register your models here.
+
+admin.site.register(AudioTrackDetail)
+admin.site.register(UserExtraDetail)
+admin.site.register(OrderDetail)
